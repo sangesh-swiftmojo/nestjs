f5804bfc76db862ab1f8e4f08c66ffea173907f7
sangesh-t2s <sangesh@foodhub.com>
2025-09-17 16:13:26 +0530

    Added logout with local strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,33 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Request, Post, UseGuards, Response } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+// import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
   }
+
+  @UseGuards(LocalAuthGuard)
+  @Get('auth/logout')
+  logout(@Request() req, @Response() res) {
+    req.logout((err) => {
+      if (err) {
+        return res.status(500).send({ message: 'Logout failed', error: err });
+      }
+      res.redirect('/'); // or any other response you want to send
+    });
+  }
+
+
+// @UseGuards(LocalAuthGuard)
+// @Post('auth/logout')
+// async logout(@Request() req) {
+//   return req.logout();
+// }
+
 }
