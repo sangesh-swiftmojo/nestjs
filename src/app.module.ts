d8570ef8e63f571679a735a2b2267cf19a28c3a8
sangesh-t2s <sangesh@foodhub.com>
2025-09-17 15:17:56 +0530

    Completed passport implementation with static json

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AgentModule } from './agent/agent.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AgentModule],
+  imports: [AgentModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
